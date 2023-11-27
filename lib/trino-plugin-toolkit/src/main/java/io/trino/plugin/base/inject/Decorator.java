a2228895fd089281f7db144a7ff13897f392be51
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2023-11-27 19:01:27 +0100

    Add DecoratorBinder

---
+++ lib/trino-plugin-toolkit/src/main/java/io/trino/plugin/base/inject/Decorator.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.plugin.base.inject;
+
+@FunctionalInterface
+public interface Decorator<T>
+{
+    T apply(T value);
+
+    default boolean appliesTo(T value)
+    {
+        return true;
+    }
+
+    default int priority()
+    {
+        return 0;
+    }
+
+    static <T> Decorator<T> nonModifyingDecorator()
+    {
+        return new Decorator<>()
+        {
+            @Override
+            public T apply(T value)
+            {
+                return value;
+            }
+
+            @Override
+            public boolean appliesTo(T value)
+            {
+                return false;
+            }
+
+            @Override
+            public int priority()
+            {
+                return -1;
+            }
+
+            @Override
+            public String toString()
+            {
+                return "nonModifyingDecorator";
+            }
+        };
+    }
+}
