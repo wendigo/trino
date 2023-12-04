d89988e220b3806119ba76ab3d88da51819f4dc4
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2023-12-04 16:58:52 +0100

    Add support for graceful coordinator shutdown

--- core/trino-main/src/main/java/io/trino/dispatcher/QueuedStatementResource.java
+++ core/trino-main/src/main/java/io/trino/dispatcher/QueuedStatementResource.java
@@ -35,6 +35,7 @@ import io.trino.server.HttpRequestSessionContextFactory;
 import io.trino.server.ProtocolConfig;
 import io.trino.server.ServerConfig;
 import io.trino.server.SessionContext;
+import io.trino.server.StartupStatus;
 import io.trino.server.protocol.QueryInfoUrlFactory;
 import io.trino.server.protocol.Slug;
 import io.trino.server.security.InternalPrincipal;
@@ -96,6 +97,7 @@ import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 import static jakarta.ws.rs.core.MediaType.TEXT_PLAIN_TYPE;
 import static jakarta.ws.rs.core.Response.Status.BAD_REQUEST;
 import static jakarta.ws.rs.core.Response.Status.FORBIDDEN;
+import static jakarta.ws.rs.core.Response.Status.GONE;
 import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
@@ -122,9 +124,11 @@ public class QueuedStatementResource
     private final boolean compressionEnabled;
     private final Optional<String> alternateHeaderName;
     private final QueryManager queryManager;
+    private final StartupStatus status;
 
     @Inject
     public QueuedStatementResource(
+            StartupStatus status,
             HttpRequestSessionContextFactory sessionContextFactory,
             DispatchManager dispatchManager,
             Tracer tracer,
@@ -134,6 +138,7 @@ public class QueuedStatementResource
             ProtocolConfig protocolConfig,
             QueryManagerConfig queryManagerConfig)
     {
+        this.status = requireNonNull(status, "status is null");
         this.sessionContextFactory = requireNonNull(sessionContextFactory, "sessionContextFactory is null");
         this.dispatchManager = requireNonNull(dispatchManager, "dispatchManager is null");
         this.tracer = requireNonNull(tracer, "tracer is null");
@@ -168,6 +173,10 @@ public class QueuedStatementResource
     {
         if (isNullOrEmpty(statement)) {
             throw badRequest(BAD_REQUEST, "SQL statement is empty");
+        }
+
+        if (status.isShutdownStarted()) {
+            return badRequest(GONE, "Coordinator is shutting down").getResponse();
         }
 
         Query query = registerQuery(statement, servletRequest, httpHeaders);
@@ -391,7 +400,7 @@ public class QueuedStatementResource
             long lastToken = this.lastToken.get();
             // token should be the last token or the next token
             if (token != lastToken && token != lastToken + 1) {
-                throw new WebApplicationException(Response.Status.GONE);
+                throw new WebApplicationException(GONE);
             }
             // advance (or stay at) the token
             this.lastToken.compareAndSet(lastToken, token);
